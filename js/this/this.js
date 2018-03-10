efa8e96a48fcdac33411ae9bf7a252d8e1cd6ccf
cc <245001402@qq.com>
2018-03-10 21:00:14 +0800

    实验室效率好低啊，回去继续...

--- js/this/this.js
+++ js/this/this.js
@@ -1,3 +1,4 @@
+//一、函数中this的指向
 //情况1：函数中使用了this，但是没有被上一级对象所调用，则this指向window对象 strict模式下this为undefined
 function test1() {
 	var name = 'cc';
@@ -27,29 +28,44 @@ var test3 = {
 }
 test3.b.getA() //15 (方法由b对象调用)
 
-var test4 = {
-	a: 10,
-	b: {
-		a: 15,
-		getA: function () {
-			console.log(this.a)
-		}
-	}
-}
-
-var obj = test4.b.getA
+var obj = test3.b.getA
 obj() //undefined (obj执行的时候是由window对象调用，故this.a为window.a)
 
+//二、构造函数
+function Hello(msg) {
+	this.msg = 'hello'+ msg
+}
+let obj = new Hello('cc');
+console.log(obj.msg) //result:hello cc 
+//使用new去调用一个构造函数时，内部的this指向的是实例化出来的对象
+Hello('dd');
+console.log(window.msg)//result: hello dd; Hello函数由window对象调用
 
 
+//三、call&apply&bind
+let showThis = function () {
+	console.log(this);
+}
 
+showThis() // window
+showThis.call(undefined) // window 非strict模式下第一个参数为undefined和null，this指向全局对象
+showThis.call(null) // window
+showThis.call({name: 'aaa'}) // {name: 'aaa'}
 
+//解决上述情况3的方法：
+var test4 = {
+	a: 10,
+	b: function () {
+		console.log(this.a)
+	}
+}
+test4.b() //10 (方法由test4对象调用)
+setTimeout(test4.b,1000) //undefined  setTimeout由window对象调用执行
 
-
-
-
-
-
+var another = function () {
+	test4.b.call(test4)
+}
+setTimeout(another, 1000) //10 
 
 
 
